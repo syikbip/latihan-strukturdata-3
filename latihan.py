3456d5ac77d9b0a31e270a7b861929cbfcf8c7bb
syikbip <syikbip@gmail.com>
2026-02-28 06:29:39 +0700

    Commit all files to remove submodule/empty folder

--- latihan.py
+++ latihan.py
@@ -37,3 +37,13 @@ class Rekening:
 
 x = Person("Syikbi", "Laki-laki",30)
 x.printname()
+
+data_aktivitas = [("Diki", 88), ("Aqul", 45), ("Abid", 92), ("Rehan", 70)]
+
+for nama, poin in data_aktivitas:
+    if poin > 80:
+        print(f"{nama} mendapatkan predikat Gold")
+    elif 50 <= poin <= 80:
+        print(f"{nama} mendapatkan predikat Silver")
+    else:
+        print(f"{nama} mendapatkan predikat Bronze")
